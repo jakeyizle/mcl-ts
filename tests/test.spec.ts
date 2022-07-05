0a0b1e889a1d619d78b4aabe386c5afe64dad110
jakeyizle <whiterbread@gmail.com>
2022-07-04 23:28:40 -0400

    test tests in github action

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -46,34 +46,7 @@ test('Main window state', async () => {
 
 test('Main window web content', async () => {
   const page = await electronApp.firstWindow();
-  const element = await page.$('#app', {strict: true});
+  const element = await page.$('#root', {strict: true});
   expect(element, 'Can\'t find root element').toBeDefined();
   expect((await element!.innerHTML()).trim(), 'Window content was empty').not.equal('');
-});
-
-
-test('Preload versions', async () => {
-  const page = await electronApp.firstWindow();
-  const renderedVersions = await page.locator('#process-versions').innerText();
-
-  const expectedVersions = await electronApp.evaluate(() => process.versions);
-
-  for (const expectedVersionsKey in expectedVersions) {
-    expect(renderedVersions).include(`${expectedVersionsKey}: v${expectedVersions[expectedVersionsKey]}`);
-  }
-});
-
-
-test('Preload nodeCrypto', async () => {
-  const page = await electronApp.firstWindow();
-
-  /**
-   * Random string to test hashing
-   */
-  const testString = Math.random().toString(36).slice(2, 7);
-
-  await page.fill('input', testString);
-  const renderedHash = await page.inputValue('input[readonly]');
-  const expectedHash = createHash('sha256').update(testString).digest('hex');
-  expect(renderedHash).toEqual(expectedHash);
 });
